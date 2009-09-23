b80fc239163a67dfa96ef5921dcaa850a6482cb4
Jared Morgan <jmorgan@morgancreative.net>
2009-09-23 16:12:54 -0500

    Delete unneeded files (generated by Rails plugin generated, IIRC)

--- init.rb
+++
@@ -1,3 +0,0 @@
-# Include hook code here
-require File.dirname(__FILE__) + '/lib/ach_file.rb'
-require File.dirname(__FILE__) + '/lib/batch.rb'
--- tasks/ach_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :ach do
-#   # Task goes here
-# end
