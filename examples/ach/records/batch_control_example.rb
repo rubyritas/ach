706c4220bbc9836ae1d0582c3fac0802c30d6f18
Jared Morgan <jmorgan@morgancreative.net>
2009-09-26 13:36:19 -0500

    Very simple test for Records::BatchControl

---
+++ examples/ach/records/batch_control_example.rb
@@ -0,0 +1,34 @@
+require 'example_helper'
+
+describe ACH::Records::BatchControl do
+  before(:each) do
+    @record = ACH::Records::BatchControl.new
+    @record.entry_count = 1
+    @record.entry_hash = 2
+    @record.debit_total = 3
+    @record.credit_total = 4
+    @record.company_identification = '123456789'
+    @record.message_authentication_code = '22345678'
+    @record.originating_dfi_identification = '32345678'
+    @record.batch_number = 5
+  end
+  
+  describe '#to_ach' do
+    it 'should generate record string' do
+      exp = [
+        '8',
+        '200',
+        '000001',
+        '0000000002',
+        '000000000003',
+        '000000000004',
+        '1123456789',
+        '22345678           ',
+        '      ',
+        '32345678',
+        '0000005'
+      ]
+      @record.to_ach.should == exp.join('')
+    end
+  end
+end
