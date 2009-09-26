7cc222f802c28402f581c39e57e96ccabc2c4fe4
Jared Morgan <jmorgan@morgancreative.net>
2009-09-26 13:03:46 -0500

    Add comments and example for ACH::Records::Nines

---
+++ examples/ach/records/nines_example.rb
@@ -0,0 +1,13 @@
+require 'example_helper'
+
+describe ACH::Records::Nines do
+  before(:each) do
+    @nines = ACH::Records::Nines.new
+  end
+
+  describe '#to_ach' do
+    it 'should generate 94 copies of the digit "9"' do
+      @nines.to_ach.should == ('9' * 94)
+    end
+  end
+end
--- lib/ach/records/nines.rb
+++ lib/ach/records/nines.rb
@@ -1,4 +1,12 @@
 module ACH::Records
+  # The number of records must be a multiple of ten (that is, complete 940
+  # character blocks). If needed, rows that consist of the digit "9" may be
+  # used to pad the the file.
+  #
+  # "Nines" records thus consist of a single constant field containing the digit
+  # "9" 94 times.
+  #
+  # See 2008 ACH Rules, Appx 1, Sec 1.5; Appx 2, Sec 2.3
   class Nines < Record
     @fields = []
     
