133d81f4b4b63e0a5be8f63cfaec733afdf16436
Jared Morgan <jmorgan@morgancreative.net>
2009-11-06 20:01:56 -0600

    Make BatchHeader#service_class_code a regular field
    - Validate against SERVICE_CLASS_CODES constant
    - Defaults to 200 (debits and credits)
    - Pending test for determining based on actual entries (This requires that
      the BatchHeader have a reference to the Batch)

--- examples/ach/records/batch_header_example.rb
+++ examples/ach/records/batch_header_example.rb
@@ -25,13 +25,38 @@ describe ACH::Records::BatchHeader do
   end
   
   describe '#service_class_code' do
-    it 'should accept an Integer'
-    it 'should accept a String'
-    it 'must be a 200, 220, 225 or 280'
+    it 'should accept an Integer' do
+      @record.service_class_code = 200
+      @record.service_class_code.should == 200
+      @record.service_class_code_to_ach.should == '200'
+    end
+    
+    it 'should accept a String' do
+      @record.service_class_code = '220'
+      @record.service_class_code.should == '220'
+      @record.service_class_code_to_ach.should == '220'
+    end
+    
+    it 'must be a 200, 220, 225 or 280' do
+      lambda { @record.service_class_code = '201' }.should raise_error(RuntimeError)
+      lambda { @record.service_class_code = 201 }.should raise_error(RuntimeError)
+      lambda { @record.service_class_code = '2020' }.should raise_error(RuntimeError)
+      lambda { @record.service_class_code = '20' }.should raise_error(RuntimeError)
+    end
    
     describe '#service_class_code_to_ach' do
-      it 'should use a given value'
-      it 'should default to determining from entries'
+      it 'should use a given value' do
+        @record.service_class_code = '220'
+        @record.service_class_code_to_ach.should == '220'
+        @record.service_class_code = '225'
+        @record.service_class_code_to_ach.should == '225'
+      end
+      
+      it 'should default to 200 when entries unavailable' do
+        @record.service_class_code_to_ach.should == '200'
+      end
+      
+      it 'should default to determining from entries available'
     end
   end
 end
