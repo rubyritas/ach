51f3d2a0896004b1c424aa78480e25e3e8128174
Brian Armstrong <barmstrong@gmail.com>
2012-10-10 23:34:05 -0700

    allow 10 digit immediate_origin in file header

--- lib/ach/records/file_header.rb
+++ lib/ach/records/file_header.rb
@@ -5,7 +5,7 @@ module ACH::Records
     const_field :record_type, '1'
     const_field :priority_code, '01'
     routing_field :immediate_destination
-    routing_field :immediate_origin
+    field :immediate_origin, String, nil, /\A\d{9,10}\Z/
     field :transmission_datetime, Time,
         lambda { |f| f.strftime('%y%m%d%H%M')},
         lambda { Time.now }
