cd4fe1770839f79c213bfbce1d8cb03740be7c07
Ari Pollak <ari@thelevelup.com>
2015-04-20 17:16:50 -0400

    Add calculator for valid effective date

--- lib/ach.rb
+++ lib/ach.rb
@@ -27,6 +27,7 @@ require 'iconv' if RUBY_VERSION < '1.9'
 require 'ach/field_identifiers'
 require 'ach/ach_file'
 require 'ach/batch'
+require 'ach/next_federal_reserve_effective_date'
 require 'ach/version'
 
 # Require records files
