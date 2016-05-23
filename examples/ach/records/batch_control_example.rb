46c4a8fcc768d690aaed904f5c5f9fc094265d7d
Peter Szczepanski <peter.szczepanski@avant.com>
2016-05-23 09:08:09 -0500

    Modify company identification regex to accept letters
    * Modify batch headers and control regex to accept any string with:
    first value A-Z, followed by 6-8 digits OR any string with 7-9 digits.
    * add regex unit tests
    * bump version

--- examples/ach/records/batch_control_example.rb
+++ examples/ach/records/batch_control_example.rb
@@ -39,4 +39,25 @@ describe ACH::Records::BatchControl do
     lambda { @record.company_identification_code_designator = 'A' }.should_not raise_error
   end
 
+  describe 'batch control - company identification' do
+    it 'should allow for numeric values' do
+      lambda { @record.company_identification = '012345678' }.should_not raise_error
+    end
+
+    it 'should allow for alphanumeric values, with a leading letter' do
+      lambda { @record.company_identification = 'A12345678' }.should_not raise_error
+    end
+
+    it 'should not allow for multiple letters' do
+      lambda { @record.company_identification = 'AA1234567' }.should raise_error
+    end
+
+    it 'should not allow invalid length, 6 digits' do
+      lambda { @record.company_identification = '123456' }.should raise_error
+    end
+
+    it 'should not allow invalid length, 10 digits' do
+      lambda { @record.company_identification = '1234567890' }.should raise_error
+    end
+  end
 end
