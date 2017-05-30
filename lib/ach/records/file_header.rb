412e64613c230fce1a534f457dba7b8fc0fb0ad7
bthews <bthews@gmail.com>
2017-05-30 09:40:39 -0400

    IMS-10957 allow appending space to immediate origin

--- lib/ach/records/file_header.rb
+++ lib/ach/records/file_header.rb
@@ -5,7 +5,7 @@ module ACH::Records
     const_field :record_type, '1'
     const_field :priority_code, '01'
     field :immediate_destination, String, lambda { |f| f.rjust(10) }, nil, /\A(\d{9,10}|)\z/
-    field :immediate_origin, String, lambda { |f| f.rjust(10) }, nil, /\A[A-Z\d\s]{1}?\d{9}\z/
+    field :immediate_origin, String, lambda { |f| f.rjust(10) }, nil, /\A[A-Z\d\s]{1}?\d{9}\s?\z/
     field :transmission_datetime, Time,
         lambda { |f| f.strftime('%y%m%d%H%M')},
         lambda { Time.now }
