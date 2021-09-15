7db9e0407e9423235ebcc03dcb6612aedb051910
Dan Rench <daniel.rench@amount.com>
2021-09-15 16:50:23 -0500

    Clean up spec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
-require 'rubygems'
-require 'bundler/setup'
-Bundler.require
 require 'ach'
 require 'date'
-
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-end
