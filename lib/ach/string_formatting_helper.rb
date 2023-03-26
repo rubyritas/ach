96c9296b42f0143b9688abebfb2ff46eb0c457cf
Jared Morgan <jmorgan@morgancreative.net>
2023-03-25 19:58:40 -0500

    Fix warning and spec failure related to company_descriptive_date

--- lib/ach/string_formatting_helper.rb
+++ lib/ach/string_formatting_helper.rb
@@ -1,12 +1,15 @@
 module ACH
   module StringFormattingHelper
-    # Passing in SD to the date signifies same-day to banks. This is used for the company_descriptive_date
+    # Passing in SD to the date signifies same-day to banks. This is used for
+    # the company_descriptive_date
     def self.stringify_with_same_day(f)
       return f.upcase if f.to_s.upcase.match(/^SD\d+$/)
 
-      f.strftime('%y%m%d')
-    rescue
-      f
+      if f.respond_to?(:strftime)
+        f = f.strftime('%y%m%d')
+      end
+
+      f[0..5].rjust(6)
     end
   end
 end
